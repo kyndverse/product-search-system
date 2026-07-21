4d87378cde2cbd2e70190866c85865f5cdb67ada
IhsanJati <ihsanjati127@gmail.com>
2026-07-21 11:56:06 +0700

    feat: add global validation pipe and HTTP exception filter to main application bootstrap

--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { validationConfig } from './common/config/validation.config';
+import { HttpExceptionFilter } from './common/filters/http-exeption.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe(validationConfig));
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
